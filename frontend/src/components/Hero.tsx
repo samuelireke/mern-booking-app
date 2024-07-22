eac3fea2b24f7121c50db39c9c07496400f37d0b
samuelireke <samuelireke@gmail.com>
2024-07-22 17:57:37 +0100

    frontent app layout implemented

---
+++ frontend/src/components/Hero.tsx
@@ -0,0 +1,15 @@
+const Hero = () => {
+  return (
+    <div className="bg-blue-800 pb-16">
+      <div className="container mx-auto flex flex-col gap-2">
+        <h1 className="text-5xl text-white font-bold">Find your next stay</h1>
+        <p className="text-2xl text-white">
+          Discover the world's most amazing low priced destinations and book
+          your perfect hotel now!
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
