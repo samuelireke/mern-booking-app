0a58c5465712b3118ecde90985e4719cf6e1556a
samuelireke <samuelireke@gmail.com>
2024-07-24 18:58:39 +0100

    user validation added in backend

---
+++ backend/src/middleware/validators/user.validator.ts
@@ -0,0 +1,28 @@
+// middlewares/validators/userValidator.js
+import { NextFunction, Request, Response } from "express";
+import { check, validationResult } from "express-validator";
+
+const validateUser = [
+  check("firstName", "First Name is required").isString(),
+  check("lastName", "Last Name is required").isString(),
+  check("email", "Enter a valid email").isEmail(),
+  check(
+    "password",
+    "Password must contain at least 8 characters, including a mix of uppercase, lowercase, numbers, and symbols."
+  ).isStrongPassword({
+    minLength: 8,
+    minLowercase: 1,
+    minUppercase: 1,
+    minNumbers: 1,
+    minSymbols: 1,
+  }),
+  (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    next();
+  },
+];
+
+export default validateUser;
--- backend/src/routes/user.route.ts
+++ backend/src/routes/user.route.ts
@@ -1,8 +1,9 @@
 import express from "express";
 import { registerUser } from "../controllers/user.controller";
+import validateUser from "../middleware/validators/user.validator";
 
 const router = express.Router();
 
-router.post("/register", registerUser);
+router.post("/register", validateUser, registerUser);
 
 export default router;
